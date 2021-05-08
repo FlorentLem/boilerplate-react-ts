bdde19aa62638d9d5243b5bdba1bbff09bb4b072
FlorentLem <florent@vianova.io>
2021-05-08 20:25:47 +0200

    added CRUD for users

--- routes/api/users.routes.js
+++ routes/api/users.routes.js
@@ -1,8 +1,47 @@
-const router = require('express').Router();
-const User = require('../../models/User');
+const router = require("express").Router();
+const User = require("../../models/User");
 
-router.get('/', (_, res) => {
-  res.status(200).json({ message: 'Hello boilerplate!' });
+router.post("/create", (req, res) => {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(() => {
+      User.find({}, (err, users) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(500)
+            .json({ results: [], message: "Error while fetching users" });
+          return;
+        }
+        res.status(200).json({ results: users, message: "User was created" });
+      });
+    })
+    .catch((err) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ results: [], message: "Error while creating user" });
+        return;
+      }
+    });
+});
+
+router.get("/", (_, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ results: [], message: "Error while fetching users" });
+      return;
+    }
+    res.status(200).json({ results: users, message: "All user fetched" });
+  });
 });
 
 module.exports = router;
